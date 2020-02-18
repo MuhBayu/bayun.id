bf8fc8d13a3121352bd063d41b3834674f1a9017
MuhBayu <bnugraha00@hotmail.com>
2020-02-18 13:21:58 +0700

    add build

---
+++ build/precache-manifest.d072df1d72043b0c47a3ccb87d932683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8c365b1a458d10eaec940e44136882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298d435a3162a63a1ae0",
+    "url": "/static/css/main.e0ed50c5.chunk.css"
+  },
+  {
+    "revision": "f7bd6c6cd00bb60cb2f3",
+    "url": "/static/js/2.90a0bc4a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.90a0bc4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298d435a3162a63a1ae0",
+    "url": "/static/js/main.131de8e6.chunk.js"
+  },
+  {
+    "revision": "53c72eef00eb6ff8e25c",
+    "url": "/static/js/runtime-main.84ae625e.js"
+  }
+]);
